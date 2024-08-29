e229614a6ec1ed062f660ee60be81c6173153c34
Alan Yang <99234680+OlonYong@users.noreply.github.com>
2024-08-29 21:13:51 +1200

    Reminder function
    Updating the reminder function to work with accounts.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import datetime
 import openpyxl
 from PIL import ImageTk, Image
@@ -8,8 +8,15 @@ import webbrowser
 
 
 root = Tk()
-	@@ -13,8 +15,8 @@
+root.geometry("1000x550")
+root.resizable(False, False)
 
+loaded_images = []
+global user
+global passw
+user = ""
+passw = ""
+    
 def main():
     m_frame = Frame(root)
     m_frame.pack()
@@ -17,25 +24,39 @@ def main():
     buffer.grid(row=0, column=0)
 
     title = Label(m_frame, text="Welcome!", font=("Helvetica", 40, "bold"), fg="green")
-	@@ -28,57 +30,92 @@ def main():
+    title.grid(row=0, column=2, padx=(0, 0))
+
+    reminder = Button(m_frame, width=25, height=13, text="Next collection day", command= lambda:[m_frame.destroy(), collection()], font=(15))
+    reminder.grid(column=1, row=1, padx=10, pady=10)
+    reminder.grid_propagate(False)
+
+    guide = Button(m_frame, width=25, height=13, text="Recycling guide", command= lambda:[m_frame.destroy(), guides()], font=(15))
     guide.grid(column=2, row=1, padx=10, pady=10)
     guide.grid_propagate(False)
 
-    account = Button(m_frame, width=30, height=15, text="Manage Account", command= lambda:[m_frame.destroy(), login()])
+    account = Button(m_frame, width=25, height=13, text="Manage Account", command= lambda:[m_frame.destroy(), login()], font=(15))
     account.grid(column=3, row=1, padx=10, pady=10)
     account.grid_propagate(False)
-
+    
 def collection():
     c_frame = Frame(root)
     c_frame.pack()
-
-    if os.path.exists("dates.txt"):
-        with open("dates.txt", "r") as file:
-            for line in file:
-                year, month, day, is_recycling = line.split("-")
-            if datetime.datetime.now() < datetime.datetime(int(year), int(month), int(day)):
-                reminder()
-                return
+    print(user, passw)
+    if user == "" and passw == "":
+        messagebox.showinfo("Important", "Please make an account to save your data.")
+    else:
+        if os.path.exists("iteration1/dates.txt"):
+            with open("iteration1/dates.txt", "r") as file:
+                for line in file:
+                    dates_data, username = line.split(" : ")
+                    year, month, day, is_recycling = dates_data.split("-")
+                    print(username, dates_data)
+                    if username == user:
+ #                       if datetime.datetime.now() >= datetime.datetime(int(year), int(month), int(day)):
+                        c_frame.forget
+                        reminder(day, month, is_recycling)
+                        return
+                    
 
     heading = Label(c_frame, text="Enter your next collection date", font=("Helvetica", 20, "bold"))
     heading.pack()
@@ -46,7 +67,7 @@ def collection():
     Label(date, text="Day").grid(row=0, column=0)
 
     day_var = StringVar()
-    day = ttk.Combobox(date, textvariable=day_var)
+    day = ttk.Combobox(date, textvariable=day_var, state="readonly")
     day["values"] = tuple(str(i) for i in range(1, 32)) 
     day.grid(row=1, column=0)
 
@@ -54,63 +75,88 @@ def collection():
 
     Label(date, text="Month").grid(row=0, column=2)
     month_var = StringVar()
-    month = ttk.Combobox(date, textvariable=month_var)
+    month = ttk.Combobox(date, textvariable=month_var, state="readonly")
     month["values"] = tuple(str(i) for i in range(1, 13))
     month.grid(row=1, column=2)
-
+    
 
     check_recycling = IntVar()
     is_recycling = Checkbutton(c_frame, text="Is there recycling?", variable=check_recycling)
     is_recycling.pack()
 
+    submit = Button(c_frame, text="Submit", command= lambda:calculate_day(day_var.get(), month_var.get(), check_recycling.get(), c_frame))
+    submit.pack(pady=5)
+
     Button(c_frame, command= lambda: webbrowser.open_new("https://www.aucklandcouncil.govt.nz/rubbish-recycling/rubbish-recycling-collections/Pages/rubbish-recycling-collection-days.aspx"), text="Don't know your collection? Check here!").pack(pady=5)
 
-    submit = Button(c_frame, text="Submit", command= lambda:[calculate_day(day_var.get(), month_var.get(), check_recycling.get()), c_frame.destroy(), reminder()])
-    submit.pack()
-
     back = Button(c_frame, text="Back", command= lambda:[c_frame.destroy(), main()])
-    back.pack()
+    back.pack(pady=5)
 
 
-def calculate_day(day, month, is_recycling):
+def calculate_day(day, month, is_recycling, c_frame):
     collection_day = int(day)
     collection_month = int(month)
     recycling = is_recycling
-    next_date = datetime.datetime(datetime.datetime.now().year, collection_month, collection_day )
-    next_date += datetime.timedelta(days=7)
+    
+    try:
+        next_date = datetime.datetime(datetime.datetime.now().year, collection_month, collection_day )
+        next_date += datetime.timedelta(days=7)
+    except ValueError:
+        messagebox.showinfo("Important", "Please enter a valid date")
+        return
+    
     if recycling:
         recycling = False
     elif recycling == False:
         recycling = True
 
-    with open("dates.txt", "w") as file:
-        file.write(str(next_date.date())+"-"+str(recycling))
+    if user != "" and passw != "":
+        
+        with open("iteration1/dates.txt", "w") as file:
+            file.write(f"{str(next_date.date())+"-"+str(recycling)} : {user}")
+    
+    while datetime.datetime.now() > next_date:
+        next_date += datetime.timedelta(days=7)
 
-    return collection_day, collection_month, recycling
+    collection_day = next_date.day
+    collection_month = next_date.month
+    if c_frame != None:
+        c_frame.forget()
+    reminder(collection_day, collection_month, recycling)
+    
 
-def reminder():
+def reminder(day, month, is_recycling):
+    c_frame = None
     r_frame = Frame(root)
     r_frame.pack()
+    year = datetime.date.today().year
+    if user != "":
+        with open("iteration1/dates.txt", "r") as file:
+            for line in file:
+                year, month, day, is_recycling = line.split("-")
+            if datetime.datetime.now() > datetime.datetime(int(year), int(month), int(day)):
+                r_frame.forget()
+                calculate_day(day, month, is_recycling, c_frame)
+                
 
-    with open("dates.txt", "r") as file:
-        for line in file:
-            year, month, day, is_recycling = line.split("-")
-        if datetime.datetime.now() > datetime.datetime(int(year), int(month), int(day)):
-            calculate_day(day, month, is_recycling)
-
-    Label(r_frame, text="Your next collection day is:").pack()
-    Label(r_frame, text=f"{day}/{month}/{year}").pack()
+    Label(r_frame, text="Your next collection day is:", font=(15)).pack(pady=5)
+    Label(r_frame, text=f"{day}/{month}/{year}", font=(20)).pack()
     if is_recycling == True:
-        Label(r_frame, text="Recycling: Yes").pack()
+        Label(r_frame, text="Recycling: Yes", font=(20)).pack(pady=5)
     else:
-        Label(r_frame, text="Recycling: No").pack()
+        Label(r_frame, text="Recycling: No", font=(20)).pack()
 
     back = Button(r_frame, text="Back", command= lambda:[r_frame.destroy(), main()])
     back.pack()
 
 def guides():
     data = load_data()
-	@@ -91,9 +128,12 @@ def guides():
+    g_frame = Frame(root)
+    g_frame.pack()
+    title = Label(g_frame, text="Recycling Guides", font=("Helvetica", 20, "bold"))
+    title.pack()
+    catalogue = Frame(g_frame)
+    catalogue.pack()
     frame = []
     infopanels = info_panels(data)
     for i in range(6):
@@ -122,15 +168,31 @@ def guides():
 
 def load_data():
     guide_data = []
-    path = "testdatabase.xlsx"
-	@@ -113,7 +153,6 @@ def info_panels(data):
+    path = r"iteration1\testdatabase.xlsx"
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook.active
+    for row in sheet.iter_rows(min_row=0, max_col=3, max_row=6, values_only=True):
+        row = list(row)
+        image = Image.open(f"{row[2]}")
+        image = ImageTk.PhotoImage(image)
+        loaded_images.append(image)
+        row[2] = image
+        
+        guide_data.append(row)
+    return guide_data
+            
+def info_panels(data):
     info_panels = []
     for i in range(0, len(data)):
         frame = Frame(root)
         Label(frame, text=data[i][0], font=("Helvetica", 40, "bold")).pack()
         Label(frame, image=data[i][2]).pack()
         Label(frame, text=data[i][1], wraplength=500, pady=10).pack()
-	@@ -125,6 +164,136 @@ def load_panels(panel):
+        info_panels.append(frame)
+    return info_panels
+
+def load_panels(panel):
+    panel.pack()
     back = Button(panel, text="Back", command= lambda:[panel.destroy(), guides()])
     back.pack()
 
@@ -153,34 +215,41 @@ def profile():
     back = Button(p_frame, text="Back", command= lambda:[p_frame.destroy(), main()])
     back.grid(row=4, columnspan=2)
 
+    Button(p_frame, text="Log Out", command= lambda:[log_out(), p_frame.destroy()]).grid(row=3, columnspan=2, pady=5)
+
     delete = Button(p_frame, text="Delete Account", command= lambda:[delete_account(), p_frame.destroy()])
     delete.grid(row=5, columnspan=2)
 
+def log_out():
+    global user
+    global passw
+    user = ""
+    passw = ""
+    main()
+    
 def delete_account():
     d_frame = Frame(root)
     d_frame.pack()
 
     Label(d_frame, text="Are you sure you want to delete your account?").pack()
 
-    confirm = Button(d_frame, text="Yes", command= lambda:[d_frame.destroy(), main(), os.remove("info.txt")])
+    confirm = Button(d_frame, text="Yes", command= lambda:[d_frame.destroy(), main(), os.remove("iteration1/info.txt")])
     confirm.pack()
 
     back = Button(d_frame, text="No", command= lambda:[d_frame.destroy(), profile()])
     back.pack()
-
+        
 
 def login():
     try:
-        f = open('info.txt', 'r')
+        f = open('iteration1/info.txt', 'r')
         f.close()
     except FileNotFoundError:
         register_account()
         return
-
+    
     global user
     global passw
-    user = ""
-    passw = ""
 
     if user != "" and passw != "":
         profile()
@@ -190,7 +259,7 @@ def login():
     l_frame.pack()
 
     Label(l_frame, text="Login", font=("Helvetica", 20, "bold")).grid(row=0, columnspan=2)
-
+    
     Label(l_frame, text="Username").grid(row=1, column=0)
 
     username = Entry(l_frame)
@@ -215,15 +284,18 @@ def login():
     back.grid(row=7, columnspan=2, pady=(10,0))
 
 def check_login(username, password, l_frame):
-    with open("info.txt", "r") as file:
+    with open("iteration1/info.txt", "r") as file:
         for line in file:
             info = line.strip().split(" : ")
-            user = username.get()
-            passw = password.get()
-            if info[0] == user and info[1] == passw:
+            usern = username.get()
+            pas = password.get()
+            if info[0] == usern and info[1] == pas:
                 l_frame.destroy()
-                profile()
-
+                main()
+                global user
+                global passw
+                user = usern
+                passw = pas
                 return
     info_message = Label(l_frame, text="Incorrect username or password")
     info_message.grid(row=4, columnspan=2, pady=5)
@@ -233,14 +305,14 @@ def register_account():
     r_frame.pack()
 
     Label(r_frame, text="Register", font=("Helvetica", 20, "bold")).grid(row=0, columnspan=2)
-
+    
     Label(r_frame, text="Username").grid(row=1, column=0)
 
     username = Entry(r_frame)
     username.grid(row=1, column=1)
-
+    
     Label(r_frame, text="Password").grid(row=2, column=0)
-
+    
     password = Entry(r_frame, show="*")
     password.grid(row=2, column=1)
 
@@ -252,7 +324,7 @@ def register_account():
 
     back = Button(r_frame, text="Back", command= lambda:[r_frame.destroy(), main()])
     back.grid(row=5, columnspan=2)
-
+    
     Label(r_frame, text="Already have an account?").grid(row=6, columnspan=2, pady=(10,0))
     Button(r_frame, text="Login", command= lambda:[login(), r_frame.destroy()]).grid(row=7, columnspan=2)
 
@@ -261,8 +333,12 @@ def create_account(username, password, error_message, r_frame):
         error_message.config(text="Please enter a username and password", wraplength=140, pady=5)
         return
     r_frame.destroy()
-    with open("info.txt", "a") as file:
+    with open("iteration1/info.txt", "a") as file:
         file.write(f"{username} : {(password)}\n")
+    global user
+    global passw
+    user = username
+    passw = password
     main()
 
 main()
